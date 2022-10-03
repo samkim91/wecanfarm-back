0a618ff6096ba7ac5d6fc2c9c537d68494a3dd4c
soogong-myeonghwan <myeonghwan@soogong.co.kr>
2022-10-03 20:29:41 +0900

    [Feat] set allowCredentials true

--- src/main/kotlin/com/example/wecanfarm/config/WebConfig.kt
+++ src/main/kotlin/com/example/wecanfarm/config/WebConfig.kt
@@ -11,6 +11,7 @@ class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:3000", "https://we-can-farm.vercel.app/")
+            .allowCredentials(true)
             .allowedMethods(HttpMethod.GET.name, HttpMethod.POST.name, HttpMethod.PATCH.name, HttpMethod.PUT.name, HttpMethod.DELETE.name)
             .maxAge(3600)
 
